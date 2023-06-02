551940c488184be9834123fca1b7339c02e65e18
krithika117 <krithikanithyanandam1@gmail.com>
2023-06-02 11:48:51 +0530

    [Add] Search feature enabled

--- src/pages/CharacterList.jsx
+++ src/pages/CharacterList.jsx
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     fetchData("https://rickandmortyapi.com/api/character");
@@ -25,12 +27,36 @@ const CharacterList = () => {
       console.log('Error fetching data:', error);
     }
   };
+  
+  const handleSearch = async (event) => {
+    const searchValue = event.target.value;
+    setSearchTerm(searchValue);
+  
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${searchValue}`);
+      const data = response.data;
+      setSearchResults(data.results);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
 
+  const charactersToDisplay = searchTerm ? searchResults : characters;
+  
   return (
-    <div className="container mt-3">
-      <h1 className="my-5 fw-bolder">Rick and Morty Characters</h1>
+    <div className="container my-3">
+      <h1 className="mt-5 mb-2 fw-bolder">Rick and Morty Characters</h1>
+      <div className="my-4">
+        <input
+          type="text"
+          className="form-control bg-dark text-white"
+          placeholder="Search characters"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        {characters.map((character) => (
+        {charactersToDisplay.map((character) => (
           <div key={character.id} className="col">
             <CardComponent character={character} />
           </div>
